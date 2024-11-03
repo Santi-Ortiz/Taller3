50f2cc2ef3421303a1250c7879de946c03a7bce6
Santi-Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-11-02 22:27:19 -0500

    Cambios en registro

--- app/src/main/java/com/example/taller3/Registro.kt
+++ app/src/main/java/com/example/taller3/Registro.kt
@@ -15,11 +15,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.auth
+import com.google.firebase.database.database
 
 class Registro : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth;
     private lateinit var binding: ActivityRegistroBinding
+
+    companion object {
+        const val PATH_USERS = "usuarios/"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +52,22 @@ class Registro : AppCompatActivity() {
         }
     }
 
+    private fun escribirUsuario(){
+        val user = Usuario()
+        val database = Firebase.database
+
+        user.nombre = binding.nombreRegister.text.toString()
+        user.apellido = binding.apellidoRegister.text.toString()
+        user.email = binding.emailRegister.text.toString()
+        user.password = binding.passwordRegister.text.toString()
+        user.identificacion = binding.idRegister.text.toString().toInt()
+        user.latitud = 0.0
+        user.longitud = 0.0
+        user.imageUrl = ""
+        val myRef = database.getReference(PATH_USERS+auth.currentUser!!.uid)
+        myRef.setValue(user)
+    }
+
     private fun registrarUsuario(correo: String, contrasena: String){
         auth.createUserWithEmailAndPassword(correo, contrasena)
             .addOnCompleteListener(this) { task ->
@@ -58,6 +79,7 @@ class Registro : AppCompatActivity() {
                         upcrb.setDisplayName(binding.nombreRegister.text.toString() + " " + binding.apellidoRegister.text.toString())
                         //upcrb.setPhotoUri(Uri.parse("path/to/pic"))
                         user.updateProfile(upcrb.build())
+                        escribirUsuario()
                         updateUI(user)
                     }
                 } else {
--- app/src/main/java/com/example/taller3/Usuario.java
+++ app/src/main/java/com/example/taller3/Usuario.java
@@ -4,6 +4,7 @@ public class Usuario {
     public String nombre;
     public String apellido;
     public String email;
+    public String password;
     public int identificacion;
     public double latitud;
     public double longitud;
@@ -11,11 +12,12 @@ public class Usuario {
 
     public Usuario() {}
 
-    public Usuario(String nombre, String apellido, String email, int identificacion,
+    public Usuario(String nombre, String apellido, String email, String password, int identificacion,
                 double latitud, double longitud, String imageUrl) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
+        this.password = password;
         this.identificacion = identificacion;
         this.latitud = latitud;
         this.longitud = longitud;
